b27253760d9638502799943aac7fbcb1a07a2cdf
souless94 <clwk94@gmail.com>
2022-07-31 20:04:28 +0800

    fix paths

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,7 +1,7 @@
 import { Workbox } from "https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-window.prod.mjs";
 
 if ("serviceWorker" in navigator) {
-  const wb = new Workbox("/assets/js/sw.js");
+  const wb = new Workbox("assets/js/sw.js");
 
   wb.register();
 }
